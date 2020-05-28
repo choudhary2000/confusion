1addff8fa9e3a9e1e35100a2d4a3c5f06a823d2c
Sameer Choudhary <sameerme@iitk.ac.in>
2020-05-28 12:15:29 +0530

    adding transtion

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Route, Switch , Redirect, withRouter } from 'react-router-dom';
 import { actions } from 'react-redux-form';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Header from './HeaderComponent';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
@@ -69,14 +70,18 @@ class Main extends Component{
     return (
       <div>
         <Header />
-        <Switch>
-          <Route path="/home" component={HomePage} />
-          <Route path="/aboutus" component={AboutPage}/>
-          <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes}/>} />
-          <Route path="/menu/:id" component={DishWithId} />
-          <Route exact path="/contactus" component={()=> <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>} />
-          <Redirect to="/home" />
-        </Switch>
+        <TransitionGroup>
+          <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+            <Switch location={this.props.location}>
+              <Route path="/home" component={HomePage} />
+              <Route path="/aboutus" component={AboutPage}/>
+              <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes}/>} />
+              <Route path="/menu/:id" component={DishWithId} />
+              <Route exact path="/contactus" component={()=> <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>} />
+              <Redirect to="/home" />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
         <Footer />
       </div>
     );
